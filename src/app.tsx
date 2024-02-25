3a8b413562091fc24cccf0f541ee5f1f1f72d7a6
Hans Teh <73771314+HansT01@users.noreply.github.com>
2024-02-26 01:15:45 +1000

    tailwind variable classes

--- src/app.tsx
+++ src/app.tsx
@@ -2,20 +2,20 @@
 import { Router } from '@solidjs/router'
 import { FileRoutes } from '@solidjs/start'
 import { Suspense } from 'solid-js'
-import Nav from '~/components/Nav'
 import './app.css'
 
 export default function App() {
   return (
-    <Router
-      root={(props) => (
-        <>
-          <Nav />
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
-      <FileRoutes />
-    </Router>
+    <div class='dark'>
+      <Router
+        root={(props) => (
+          <>
+            <Suspense>{props.children}</Suspense>
+          </>
+        )}
+      >
+        <FileRoutes />
+      </Router>
+    </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'primary': 'var(--primary)',
+        'primary-foreground': 'var(--primary-foreground)',
+        'background': 'var(--background)',
+        'foreground': 'var(--foreground)',
+        'secondary': 'var(--secondary)',
+        'secondary-foreground': 'var(--secondary-foreground)',
+      },
+    },
   },
-  plugins: []
-};
+  plugins: [],
+}
