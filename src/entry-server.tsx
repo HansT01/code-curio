d5281a0e34f3e835f5f01eba1ed1970e5ce7f740
Hans Teh <73771314+HansT01@users.noreply.github.com>
2024-03-02 05:54:26 +1000

    meta description

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -7,6 +7,11 @@ export default createHandler(() => (
         <head>
           <meta charset='utf-8' />
           <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <meta
+            name='description'
+            content="Explore Code Curio's interactive code projects, showcasing captivating visualizations and experiences beyond static images and videos."
+          />
+          <title>Code Curio</title>
           <link rel='icon' href='/favicon.ico' />
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin='' />
